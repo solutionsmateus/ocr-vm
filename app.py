aa73bdc9ab8b44e2b67d0240a093f98b0b91a724
Pedro Daniel <p57640487@gmail.com>
2025-10-28 12:01:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ Em letras maiúsculas e minúsculas, respeitando a regra da língua portuguesa.
 
 # Extensões de arquivo 
 VALID_EXTENSIONS = ('.jpeg', '.jpg', '.png', '.pdf')
-BATCH_SIZE = 3
+BATCH_SIZE = 2
 all_markdown_results = []
 
 def process_files():
